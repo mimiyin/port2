b53e5848bb0f7445269e79c5447f549a85cc0341
mimiyin <mimiyin@gmail.com>
2018-06-01 20:54:28 -0400

    updating port

---
+++ public/index.js
@@ -0,0 +1,151 @@
+let categories;
+let galleryView;
+let arriving;
+
+const MIN_COLOR_RATE = 0.01;
+const MAX_COLOR_RATE = 0.1;
+const MIN_SKEW = -5;
+const MAX_SKEW = 5;
+
+let albers = function (p) {
+  let background;
+  let colors = [];
+
+  p.setup = function () {
+    let els = p.selectAll('.gel');
+    for (let e = 0; e < els.length; e++) {
+      colors.push(new Project(els[e]));
+    }
+    p.noCanvas();
+    p.colorMode(p.HSB, 255);
+  }
+
+  p.draw = function () {
+    for (let c = 0; c < colors.length; c++) {
+      colors[c].run();
+    }
+  }
+
+  class Project {
+    constructor(el) {
+      this.el = el;
+      this.parent = new p5.Element(this.el.parent());
+      this.skew = {
+        x : p.random(MIN_SKEW, MAX_SKEW),
+        y : p.random(MIN_SKEW, MAX_SKEW)
+      }
+      this.skews = {
+        x : p.random(-0.01, 0.01),
+        y : p.random(-0.01, 0.01)
+      }
+      this.rgb = {
+        r: 0,
+        g: 0,
+        b: 0,
+      };
+      this.rgbs = {
+        r: p.random(MIN_COLOR_RATE, MAX_COLOR_RATE),
+        g: p.random(MIN_COLOR_RATE, MAX_COLOR_RATE),
+        b: p.random(MIN_COLOR_RATE, MAX_COLOR_RATE),
+      };
+
+      // console.log(this.el.elt);
+      this.a = this.el.elt.classList.contains('menu-bg') ? 0.75 : 0.5;
+    }
+    update() {
+      for (let c in this.rgb) {
+        this.rgb[c] += this.rgbs[c];
+        if (this.rgb[c] > 255 || this.rgb[c] < 0) this.rgbs[c] *= -1;
+      }
+
+      for (let s in this.skew) {
+        this.skew[s] += this.skews[s];
+        if (this.skew[s] < MIN_SKEW || this.skew[s] > MAX_SKEW) this.skews[s] *= -1;
+      }
+    }
+    run() {
+
+      this.update();
+      this.el.style('background-color', 'rgba(' + this.rgb.r + ',' + this.rgb.g + ',' + this.rgb.b + ',' + '0.5)');
+      let skew = 'skew(' + this.skew.x + 'deg, ' + this.skew.y + 'deg)';
+      this.el.style('transform', skew);
+      //this.parent.style('color', 'rgb(' + (255 - this.rgb.r) + ',' + (255 - this.rgb.g) + ',' + (255 - this.rgb.b) + ')');
+    }
+  }
+}
+
+
+function loadJSON(callback) {
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'projects.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // .open will NOT return a value but simply returns undefined in async mode so use a callback
+      callback(xobj.responseText);
+    }
+  }
+  xobj.send(null);
+}
+// Call to function with anonymous callback
+loadJSON(function (response) {
+  // Do Something with the response e.g.
+  categories = JSON.parse(response);
+  // Assuming json data is wrapped in square brackets as Drew suggests
+  galleryView = Ractive({
+    target: '#target',
+    template: '#template',
+    data: {
+      categories: categories
+    },
+    complete: function () {
+      new p5(albers);
+      document.getElementById('menu-button').style.transform = 'skew(' + Math.random()*5 + 'deg, ' + Math.random()*-5 + 'deg)';
+    },
+    showMenu: function () {
+      leaving = arriving;
+      arriving = undefined;
+      this.hide(leaving.id);
+      this.show('menu-container');
+    },
+    show: function (id, delay) {
+      let elt = document.getElementById(id);
+      setTimeout(function () {
+        elt.classList.remove('hide');
+        elt.classList.add('show');
+        let iframe = elt.getElementsByTagName('iframe')[0];
+        if(iframe) iframe.contentWindow.postMessage('{"method":"play"}','*');
+      }, delay ? 1000 : 0);
+    },
+    hide: function (id) {
+      let elt = document.getElementById(id);
+      elt.classList.remove('show');
+      elt.classList.add('hide');
+
+      // Pause iframes
+      let iframe = elt.getElementsByTagName('iframe')[0];
+      if(iframe) iframe.contentWindow.postMessage('{"method":"pause"}','*');
+
+    },
+    showMenu: function () {
+      leaving = arriving;
+      arriving = undefined;
+      if(leaving) this.hide(leaving.id);
+      this.show('menu-container', leaving);
+    },
+    enter: function (project) {
+      if (arriving) {
+        leaving = arriving;
+        leaving.show = false;
+      }
+      arriving = project;
+      project.show = true;
+      galleryView.update();
+
+      // Hide the menu
+      this.hide('menu-container');
+      //if(leaving) this.hide(leaving.id);
+      this.show(arriving.id, true);
+    }
+  });
+});
