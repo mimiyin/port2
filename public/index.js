e706e5b79c505c7a86215b336455b6cac35f50cb
mimiyin <mimiyin@gmail.com>
2018-06-02 21:18:25 -0400

    First draft.

--- public/index.js
+++ public/index.js
@@ -1,17 +1,22 @@
+
+
+
 let categories;
 let galleryView;
+let leaving;
 let arriving;
 
 const MIN_COLOR_RATE = 0.01;
 const MAX_COLOR_RATE = 0.1;
 const MIN_SKEW = -5;
 const MAX_SKEW = 5;
+const MAX_SKEW_RATE = 0.015;
 
-let albers = function (p) {
+let albers = function(p) {
   let background;
   let colors = [];
 
-  p.setup = function () {
+  p.setup = function() {
     let els = p.selectAll('.gel');
     for (let e = 0; e < els.length; e++) {
       colors.push(new Project(els[e]));
@@ -20,7 +25,7 @@ let albers = function (p) {
     p.colorMode(p.HSB, 255);
   }
 
-  p.draw = function () {
+  p.draw = function() {
     for (let c = 0; c < colors.length; c++) {
       colors[c].run();
     }
@@ -28,15 +33,16 @@ let albers = function (p) {
 
   class Project {
     constructor(el) {
+      this.i = 0;
       this.el = el;
       this.parent = new p5.Element(this.el.parent());
       this.skew = {
-        x : p.random(MIN_SKEW, MAX_SKEW),
-        y : p.random(MIN_SKEW, MAX_SKEW)
+        x: p.random(MIN_SKEW, MAX_SKEW),
+        y: p.random(MIN_SKEW, MAX_SKEW)
       }
       this.skews = {
-        x : p.random(-0.01, 0.01),
-        y : p.random(-0.01, 0.01)
+        x: p.random(-MAX_SKEW_RATE, MAX_SKEW_RATE),
+        y: p.random(-MAX_SKEW_RATE, MAX_SKEW_RATE)
       }
       this.rgb = {
         r: 0,
@@ -49,8 +55,7 @@ let albers = function (p) {
         b: p.random(MIN_COLOR_RATE, MAX_COLOR_RATE),
       };
 
-      // console.log(this.el.elt);
-      this.a = this.el.elt.classList.contains('menu-bg') ? 0.75 : 0.5;
+      this.a = this.el.elt.classList.contains('menu-bg') ? 0.85 : 0.5;
     }
     update() {
       for (let c in this.rgb) {
@@ -64,22 +69,22 @@ let albers = function (p) {
       }
     }
     run() {
-
       this.update();
-      this.el.style('background-color', 'rgba(' + this.rgb.r + ',' + this.rgb.g + ',' + this.rgb.b + ',' + '0.5)');
-      let skew = 'skew(' + this.skew.x + 'deg, ' + this.skew.y + 'deg)';
-      this.el.style('transform', skew);
-      //this.parent.style('color', 'rgb(' + (255 - this.rgb.r) + ',' + (255 - this.rgb.g) + ',' + (255 - this.rgb.b) + ')');
+      // Safari can only take integers for rgb
+      this.el.style('background-color', 'rgba(' + p.round(this.rgb.r) + ',' + p.round(this.rgb.g) + ',' + p.round(this.rgb.b) + ',' + this.a + ')');
+      // let skew = 'skew(' + this.skew.x + 'deg, ' + this.skew.y + 'deg)';
+      // this.el.style('transform', skew);
     }
   }
 }
 
 
+// Helper function to load JSON files
 function loadJSON(callback) {
   let xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', 'projects.json', true);
-  xobj.onreadystatechange = function () {
+  xobj.onreadystatechange = function() {
     if (xobj.readyState == 4 && xobj.status == "200") {
       // .open will NOT return a value but simply returns undefined in async mode so use a callback
       callback(xobj.responseText);
@@ -88,7 +93,7 @@ function loadJSON(callback) {
   xobj.send(null);
 }
 // Call to function with anonymous callback
-loadJSON(function (response) {
+loadJSON(function(response) {
   // Do Something with the response e.g.
   categories = JSON.parse(response);
   // Assuming json data is wrapped in square brackets as Drew suggests
@@ -98,54 +103,70 @@ loadJSON(function (response) {
     data: {
       categories: categories
     },
-    complete: function () {
+    complete: function() {
       new p5(albers);
-      document.getElementById('menu-button').style.transform = 'skew(' + Math.random()*5 + 'deg, ' + Math.random()*-5 + 'deg)';
+      document.getElementById('menu-button').style.transform = 'skew(' + Math.random() * 5 + 'deg, ' + Math.random() * -5 + 'deg)';
+
+      // Navigate directly to projects
+      if(window.location.hash) {
+        let id = window.location.hash.substring(1);
+        document.getElementById('menu-' + id).click();
+      }
     },
-    showMenu: function () {
-      leaving = arriving;
+    showMenu: function() {
+      if (arriving) leaving = arriving;
       arriving = undefined;
-      this.hide(leaving.id);
+      this.hideProject(leaving.id);
       this.show('menu-container');
     },
-    show: function (id, delay) {
-      let elt = document.getElementById(id);
-      setTimeout(function () {
-        elt.classList.remove('hide');
-        elt.classList.add('show');
-        let iframe = elt.getElementsByTagName('iframe')[0];
-        if(iframe) iframe.contentWindow.postMessage('{"method":"play"}','*');
+    showProject: function(id, delay) {
+      let self = this;
+      setTimeout(function() {
+        let elt = self.show(id);
+        // Get top media item
+        let top = elt.getElementsByClassName("media").item(0).children.item(0);
+        if (top.tagName == "IFRAME") {
+          top.contentWindow.postMessage('{"method":"play"}', '*');
+        }
       }, delay ? 1000 : 0);
     },
-    hide: function (id) {
+    hideProject: function(id) {
+      let elt = this.hide(id);
+      // Pause all iframes
+      let iframe = elt.getElementsByTagName('iframe')[0];
+      if (iframe) iframe.contentWindow.postMessage('{"method":"pause"}', '*');
+      setTimeout(function(){
+        leaving.show = false;
+        galleryView.update();
+      }, 5000);
+    },
+    show: function(id) {
+      let elt = document.getElementById(id);
+      elt.classList.remove('hide');
+      elt.classList.add('show');
+      return elt;
+    },
+    hide: function(id) {
       let elt = document.getElementById(id);
       elt.classList.remove('show');
       elt.classList.add('hide');
-
-      // Pause iframes
-      let iframe = elt.getElementsByTagName('iframe')[0];
-      if(iframe) iframe.contentWindow.postMessage('{"method":"pause"}','*');
-
+      return elt;
     },
-    showMenu: function () {
-      leaving = arriving;
-      arriving = undefined;
-      if(leaving) this.hide(leaving.id);
-      this.show('menu-container', leaving);
-    },
-    enter: function (project) {
-      if (arriving) {
-        leaving = arriving;
-        leaving.show = false;
-      }
+    enter: function(project) {
+      // Arrive at new project
       arriving = project;
-      project.show = true;
+
+      // Update model
+      arriving.show = true;
       galleryView.update();
 
       // Hide the menu
       this.hide('menu-container');
-      //if(leaving) this.hide(leaving.id);
-      this.show(arriving.id, true);
+      // Showing arriving project
+      this.showProject(arriving.id);
+      console.log("Arriving: " + arriving.id);
+      console.log(leaving ? "Leaving: " + leaving.id : "No project to leave.");
+
     }
   });
 });
